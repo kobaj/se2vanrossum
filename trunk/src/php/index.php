50a29c86f5f1174bdccfcb41b2b1072ed03ca3a3
kobaj.g@gmail.com <kobaj.g@gmail.com@4ef2876e-b092-6aa7-050e-84b874410411>
2013-03-28 03:44:14 +0000

    Making the HTML look a bit better

--- trunk/src/php/index.php
+++ trunk/src/php/index.php
@@ -1,206 +1,22 @@
 <?php
-echo '<!DOCTYPE html><html><head>' .
-'<!-- no cache -->' .
-'<meta http-equiv="pragma" content="no-cache" />' .
-'<meta http-equiv="expires" content="-1" />' .
 
-'<!-- jquery -->' .
-'<script src="js/jquery.js" type="text/javascript"></script>' .
-'<script src="js/javascript.js" type="text/javascript"></script>' .
+include 'header.php';
 
-'<!-- style -->' .
-'<LINK href="css/style.css" rel="stylesheet" type="text/css">' .
+echo '<div class="pad10">';
 
-'<!-- everything else -->' .
-'<title>ACL2 Project</title>' .
-'</head><body>';
-
-if (run_setup())
-	run_create_board();
-
-function run_create_board()
-{
-	define('INPUT_TEMPLATE', 'setup/create_board_file');
-	define('SETUP_TEMPLATE', 'setup/create_board_template');
-	
-	echo '<p>Welcome to the online ACL2 Powered create-board v.01</p>';
-	
-	echo '<form method="get" action=""><textarea name="words">' . (isset ($_GET['words']) ? $_GET['words'] : 'Put your words here.') . '</textarea><br /><input type="submit" value="Submit"></form>';
-
-	if (isset ($_GET['words']))
-	{
-		// clean things up
-		$words = preg_split("/[\s,\-_\n]+/", $_GET['words']);
-		foreach ($words as $key => $word)
-		{
-			$temp_word = trim($word);
-			if (!empty ($temp_word))
-				$words[$key] = $temp_word;
-			else
-				unset ($words[$key]);
-		}
-		$implode_words = '"' . implode('" "', $words) .'"';
-
-		// then make our setup file
-		$SETUP = do_replacement(ACL2_SRC_DIR . SETUP_TEMPLATE, array('list_words' => $implode_words));
-
-		//output the words
-		echo '<ul>';
-		foreach ($words as $word)
-			echo '<li>' . $word . '</li>';
-		echo '</ul>';
-
-		// run command
-		// NOTE for now this uses redirected input, but it will eventually
-		// use clay's ACL2 code
-		$final_call = ACL2_EXE_DIR . ' < ' . $SETUP;
-		echo '<p>Command: ' . $final_call . '</p>';
-		exec($final_call, $console_log);
-		echo '<p id="board_json">';
-		$json = '';
-		foreach ($console_log as $key => $results)
-		{
-			if(starts_with($results, 'ACL2 p!>"[[') &&
-				ends_with($results, ']]"'))
-				{
-					//then just clean it
-					$json = str_replace('ACL2 p!>"[[', '[["', $results);
-					$json = str_replace(']]"', '"]]', $json);
-					
-					//first add back in our quotes
-					$json = str_replace('],[', ';', $json);
-					$json = str_replace(',', '","', $json);
-					$json = str_replace(';', '"],["', $json);
-					
-					
-					echo $json;
-				}
-		}
-		echo '</p>';
+echo 'Welcome to the <span class="red">Team VanRossum</span> series of WordGames. Programmed in ACL2 (with some magic php sprinkled in) we are able to bring you' .
+		'a whole host of fun including a WordSearch generator, CrossWord puzzle generator, and a Solver for both.';
 		
-		echo '<p id="acl2_output">';
-		var_dump ($console_log);
-		echo '</p';
+echo '<br /><br />';
 
-		//delete setup
-		unlink($SETUP);
-	}
-}
+echo 'The individuals who make up Team VanRossume include CS students in Dr. Page\'s Software Engineering II class at the University of Oklahoma.';
+echo '<br />';
+echo '<center><span class="green">Cezar Delucca</span>&middot;<span class="green">Olufemi Fashanu</span>' .
+		'&middot;<span class="green">Jakob Griffith</span>&middot;<span class="green">Clayton Miller</span>' .
+		'&middot;<span class="green">Shane Moore</span></center>';
 
-function do_replacement($file_in, $replace_array)
-{
-	$data = file_get_contents($file_in . '.txt');
+echo '</div>';
 
-	// do tag replacements or whatever you want
-	foreach($replace_array as $key => $value)
-		$data = str_replace('{'.strtoupper($key) .'}', $value, $data);	
-	
-	//$data = str_replace('{WORDS}', $words, $data);
-	$data = str_replace('{ACL2_SRC_DIR}', ACL2_SRC_DIR, $data);
-	$data = str_replace('{ACL2_TEACHPACKS}', ACL2_TEACHPACKS, $data);
+include 'tailer.php';
 
-	//make a temp file
-	$tmpfname = tempnam(sys_get_temp_dir(), 'acl');
-
-	$handle = fopen($tmpfname, "w");
-	fwrite($handle, $data);
-	fclose($handle);
-
-	return $tmpfname;
-}
-
-function run_setup()
-{
-	$running_local = false;
-	if ($_SERVER['REMOTE_ADDR'] == '::1' || $_SERVER['REMOTE_ADDR'] == '127.0.0.1')
-		$running_local = true;
-
-	if ($running_local)
-	{
-		if (isset ($_COOKIE['ACL2_EXE_DIR']) && isset ($_COOKIE['ACL2_SRC_DIR']) && isset ($_COOKIE['ACL2_TEACHPACKS']))
-		{
-			if (file_exists($_COOKIE['ACL2_EXE_DIR']) && is_dir($_COOKIE['ACL2_SRC_DIR']) && is_dir($_COOKIE['ACL2_TEACHPACKS']))
-			{
-				//run with our commands
-				set_directories($_COOKIE['ACL2_EXE_DIR'], $_COOKIE['ACL2_SRC_DIR'], $_COOKIE['ACL2_TEACHPACKS']);
-				return true;
-			}
-			else
-			{
-				echo '<p>Problem with directories, try again.</p>';
-				unset_directories();
-			}
-		}
-		elseif (isset ($_POST['ACL2_EXE_DIR']) && isset ($_POST['ACL2_SRC_DIR']) && isset ($_POST['ACL2_TEACHPACKS']))
-		{
-			if (file_exists($_POST['ACL2_EXE_DIR']) && is_dir($_POST['ACL2_SRC_DIR']) && is_dir($_POST['ACL2_TEACHPACKS']))
-			{
-				//run with our commands
-				set_directories($_POST['ACL2_EXE_DIR'], $_POST['ACL2_SRC_DIR'], $_POST['ACL2_TEACHPACKS']);
-				return true;
-			}
-			else
-			{
-				echo '<p>Problem with directories, try again.</p>';
-				unset_directories();
-			}
-		}
-
-		//print a form to enter stuffs
-		echo '<form method="post" action="">';
-		echo '<span class="label">ACL2 EXE Directory</span><input type="text" name="ACL2_EXE_DIR" /><br />';
-		echo '<span class="label">ACL2 SRC Directory</span><input type="text" name="ACL2_SRC_DIR" /><br />';
-		echo '<span class="label">ACL2 Teachpacks</span><input type="text" name="ACL2_TEACHPACKS" /><br />';
-		echo '<input type="submit" value="Submit"></form>';
-	}
-	else
-	{
-		// not running local, use preset directories
-		$ACL2_EXE_DIR = 'C:/ACL2/run_acl2.exe';
-		$ACL2_SRC_DIR = 'C:/Users/Jakob/Desktop/School/Spring2013/se/svn/src/';
-		$ACL2_TEACHPACKS = 'C:/Users/Jakob/AppData/Roaming/Racket/planet/300/5.2.1/cache/cce/dracula.plt/8/23/teachpacks';
-
-		set_directories($ACL2_EXE_DIR, $ACL2_SRC_DIR, $ACL2_TEACHPACKS);
-		return true;
-	}
-
-	return false;
-}
-
-function set_directories($ACL2_EXE_DIR, $ACL2_SRC_DIR, $ACL2_TEACHPACKS)
-{
-	define('ACL2_EXE_DIR', $ACL2_EXE_DIR);
-	define('ACL2_SRC_DIR', $ACL2_SRC_DIR);
-	define('ACL2_TEACHPACKS', $ACL2_TEACHPACKS);
-
-	set_cookies(time() + 36000);
-}
-
-function unset_directories()
-{
-	set_cookies(time() - 36000);
-}
-
-function set_cookies($time)
-{
-	setcookie('ACL2_EXE_DIR', ACL2_EXE_DIR, $time);
-	setcookie('ACL2_SRC_DIR', ACL2_SRC_DIR, $time);
-	setcookie('ACL2_TEACHPACKS', ACL2_TEACHPACKS, $time);
-}
-
-// thanks to http://stackoverflow.com/questions/834303/php-startswith-and-endswith-functions
-function starts_with($haystack, $needle)
-{
-    return !strncmp($haystack, $needle, strlen($needle));
-}
-
-function ends_with($haystack, $needle)
-{
-    $length = strlen($needle);
-    if ($length == 0) 
-        return true;
-
-    return (substr($haystack, -$length) === $needle);
-}
-
-echo '</body></html>';
+?>
---
+++ trunk/src/php/make_play_search.php
@@ -0,0 +1,198 @@
+<?php
+include 'header.php';
+
+if (run_setup())
+	run_create_board();
+
+function run_create_board()
+{
+	define('INPUT_TEMPLATE', 'setup/create_board_file');
+	define('SETUP_TEMPLATE', 'setup/create_board_template');
+	
+	echo '<div class="pad10">';
+	
+	echo 'Enter a list of words to make a wordsearch. The words can be seperated by space, dash, comma, underscore, or newlines.';
+	
+	echo '<form method="get" action=""><center><textarea name="words">' . (isset ($_GET['words']) ? $_GET['words'] : 'Put your words here.') . '</textarea>' .
+			'<br /><input type="submit" value="Submit"></center></form>';
+
+	if (isset ($_GET['words']))
+	{
+		// clean things up
+		$words = preg_split("/[\s,\-_\n]+/", $_GET['words']);
+		foreach ($words as $key => $word)
+		{
+			$temp_word = trim($word);
+			if (!empty ($temp_word))
+				$words[$key] = $temp_word;
+			else
+				unset ($words[$key]);
+		}
+		$implode_words = '"' . implode('" "', $words) .'"';
+
+		// then make our setup file
+		$SETUP = do_replacement(ACL2_SRC_DIR . SETUP_TEMPLATE, array('list_words' => $implode_words));
+
+		//output the words
+		echo '<ul>';
+		foreach ($words as $word)
+			echo '<li>' . $word . '</li>';
+		echo '</ul>';
+
+		// run command
+		// NOTE for now this uses redirected input, but it will eventually
+		// use clay's ACL2 code
+		$final_call = ACL2_EXE_DIR . ' < ' . $SETUP;
+		//echo '<p >Command: ' . $final_call . '</p>';
+		exec($final_call, $console_log);
+		echo '<p id="board_json">';
+		$json = '';
+		foreach ($console_log as $key => $results)
+		{
+			if(starts_with($results, 'ACL2 p!>"[[') &&
+				ends_with($results, ']]"'))
+				{
+					//then just clean it
+					$json = str_replace('ACL2 p!>"[[', '[["', $results);
+					$json = str_replace(']]"', '"]]', $json);
+					
+					//first add back in our quotes
+					$json = str_replace('],[', ';', $json);
+					$json = str_replace(',', '","', $json);
+					$json = str_replace(';', '"],["', $json);
+					
+					
+					echo $json;
+					break;
+				}
+		}
+		echo '</p>';
+		
+		echo '<p id="acl2_output">';
+		var_dump ($console_log);
+		echo '</p>';
+
+		echo '</div>';
+
+		//delete setup
+		unlink($SETUP);
+	}
+}
+
+function do_replacement($file_in, $replace_array)
+{
+	$data = file_get_contents($file_in . '.txt');
+
+	// do tag replacements or whatever you want
+	foreach($replace_array as $key => $value)
+		$data = str_replace('{'.strtoupper($key) .'}', $value, $data);	
+	
+	//$data = str_replace('{WORDS}', $words, $data);
+	$data = str_replace('{ACL2_SRC_DIR}', ACL2_SRC_DIR, $data);
+	$data = str_replace('{ACL2_TEACHPACKS}', ACL2_TEACHPACKS, $data);
+
+	//make a temp file
+	$tmpfname = tempnam(sys_get_temp_dir(), 'acl');
+
+	$handle = fopen($tmpfname, "w");
+	fwrite($handle, $data);
+	fclose($handle);
+
+	return $tmpfname;
+}
+
+function run_setup()
+{
+	$running_local = false;
+	if ($_SERVER['REMOTE_ADDR'] == '::1' || $_SERVER['REMOTE_ADDR'] == '127.0.0.1')
+		$running_local = true;
+
+	if ($running_local)
+	{
+		if (isset ($_COOKIE['ACL2_EXE_DIR']) && isset ($_COOKIE['ACL2_SRC_DIR']) && isset ($_COOKIE['ACL2_TEACHPACKS']))
+		{
+			if (file_exists($_COOKIE['ACL2_EXE_DIR']) && is_dir($_COOKIE['ACL2_SRC_DIR']) && is_dir($_COOKIE['ACL2_TEACHPACKS']))
+			{
+				//run with our commands
+				set_directories($_COOKIE['ACL2_EXE_DIR'], $_COOKIE['ACL2_SRC_DIR'], $_COOKIE['ACL2_TEACHPACKS']);
+				return true;
+			}
+			else
+			{
+				echo '<p>Problem with directories, try again.</p>';
+				unset_directories();
+			}
+		}
+		elseif (isset ($_POST['ACL2_EXE_DIR']) && isset ($_POST['ACL2_SRC_DIR']) && isset ($_POST['ACL2_TEACHPACKS']))
+		{
+			if (file_exists($_POST['ACL2_EXE_DIR']) && is_dir($_POST['ACL2_SRC_DIR']) && is_dir($_POST['ACL2_TEACHPACKS']))
+			{
+				//run with our commands
+				set_directories($_POST['ACL2_EXE_DIR'], $_POST['ACL2_SRC_DIR'], $_POST['ACL2_TEACHPACKS']);
+				return true;
+			}
+			else
+			{
+				echo '<p>Problem with directories, try again.</p>';
+				unset_directories();
+			}
+		}
+
+		//print a form to enter stuffs
+		echo '<form method="post" action="">';
+		echo '<span class="label">ACL2 EXE Directory</span><input type="text" name="ACL2_EXE_DIR" /><br />';
+		echo '<span class="label">ACL2 SRC Directory</span><input type="text" name="ACL2_SRC_DIR" /><br />';
+		echo '<span class="label">ACL2 Teachpacks</span><input type="text" name="ACL2_TEACHPACKS" /><br />';
+		echo '<input type="submit" value="Submit"></form>';
+	}
+	else
+	{
+		// not running local, use preset directories
+		$ACL2_EXE_DIR = 'C:/ACL2/run_acl2.exe';
+		$ACL2_SRC_DIR = 'C:/Users/Jakob/Desktop/School/Spring2013/se/svn/src/';
+		$ACL2_TEACHPACKS = 'C:/Users/Jakob/AppData/Roaming/Racket/planet/300/5.2.1/cache/cce/dracula.plt/8/23/teachpacks';
+
+		set_directories($ACL2_EXE_DIR, $ACL2_SRC_DIR, $ACL2_TEACHPACKS);
+		return true;
+	}
+
+	return false;
+}
+
+function set_directories($ACL2_EXE_DIR, $ACL2_SRC_DIR, $ACL2_TEACHPACKS)
+{
+	define('ACL2_EXE_DIR', $ACL2_EXE_DIR);
+	define('ACL2_SRC_DIR', $ACL2_SRC_DIR);
+	define('ACL2_TEACHPACKS', $ACL2_TEACHPACKS);
+
+	set_cookies(time() + 36000);
+}
+
+function unset_directories()
+{
+	set_cookies(time() - 36000);
+}
+
+function set_cookies($time)
+{
+	setcookie('ACL2_EXE_DIR', ACL2_EXE_DIR, $time);
+	setcookie('ACL2_SRC_DIR', ACL2_SRC_DIR, $time);
+	setcookie('ACL2_TEACHPACKS', ACL2_TEACHPACKS, $time);
+}
+
+// thanks to http://stackoverflow.com/questions/834303/php-startswith-and-endswith-functions
+function starts_with($haystack, $needle)
+{
+    return !strncmp($haystack, $needle, strlen($needle));
+}
+
+function ends_with($haystack, $needle)
+{
+    $length = strlen($needle);
+    if ($length == 0) 
+        return true;
+
+    return (substr($haystack, -$length) === $needle);
+}
+
+include 'tailer.php';
