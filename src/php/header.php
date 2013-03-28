df14f5de4e60ca8ff7331f89b34d75b5b9c6bcb8
kobaj.g@gmail.com <kobaj.g@gmail.com@4ef2876e-b092-6aa7-050e-84b874410411>
2013-03-28 03:44:14 +0000

    Making the HTML look a bit better

---
+++ src/php/header.php
@@ -0,0 +1,24 @@
+<?php
+
+echo '<!DOCTYPE html><html><head>' .
+'<!-- no cache -->' .
+'<meta http-equiv="pragma" content="no-cache" />' .
+'<meta http-equiv="expires" content="-1" />' .
+
+'<!-- jquery -->' .
+'<script src="js/jquery.js" type="text/javascript"></script>' .
+'<script src="js/javascript.js" type="text/javascript"></script>' .
+
+'<!-- style -->' .
+'<LINK href="css/style.css" rel="stylesheet" type="text/css">' .
+
+'<!-- everything else -->' .
+'<title>ACL2 Project</title>' .
+'</head><body>';
+
+echo '<center><h1>ACL2 WordGames</h1></center>';
+echo '<center><h3><a href="make_play_search.php">WordSearch</a> &middot; CrossWord &middot; Solver</h3></center>';
+
+echo '<div class="center">';
+
+?>
