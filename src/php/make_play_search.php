a7c608f9e2ce44ac7c85cf193f3af062a706e20d
kobaj.g@gmail.com <kobaj.g@gmail.com@4ef2876e-b092-6aa7-050e-84b874410411>
2013-04-04 16:03:57 +0000

    adding in random letters to the full stops and fixing a few php bugs

--- src/php/make_play_search.php
+++ src/php/make_play_search.php
@@ -4,7 +4,10 @@ require_once 'php_setup.php';
 require_once 'magic_file_builder.php';
 
 if (run_setup())
+{
+	header_print();
 	run_create_board();
+}
 
 function run_create_board()
 {
@@ -13,7 +16,7 @@ function run_create_board()
 	if (isset ($_GET['words']))
 	{
 		echo 'Click letters to play the wordsearch or <a href="make_play_search.php">click here</a> to make a new board.<br /><br />';
-		
+
 		// clean things up
 		$words = preg_split("/[\s,~.\-_\n]+/", $_GET['words']);
 		foreach ($words as $key => $word)
@@ -36,7 +39,7 @@ function run_create_board()
 		foreach ($words as $word)
 			echo '<li>' . $word . '</li>';
 		echo '</ul>';
-		
+
 		echo '</td><td><center>';
 
 		// run command
@@ -44,40 +47,40 @@ function run_create_board()
 		// use clay's ACL2 code
 		$final_call = ACL2_EXE_DIR . ' < ' . $SETUP;
 		exec($final_call, $console_log);
-		
+
 		//echo '<p>Command: ' . $final_call . '</p>';
-		
+
 		$output_found = false;
 		foreach ($console_log as $key => $results)
 		{
 			$head = 'ACL2 p>("[[';
 			$tail = ']]"';
-			
+
 			if(starts_with($results, $head) &&
 				stristr($results, $tail))
 				{
 					// first the board output
 					$explode_results = explode($tail, $results);
 					$results = $explode_results[0] . $tail;
-					
+
 					echo '<p id="board_json">';
 					$json = '';
-					
+
 					//then just clean it
 					$json = str_replace($head, '[["', $results);
 					$json = str_replace($tail, '"]]', $json);
-					
+
 					//first add back in our quotes
 					$json = str_replace('],[', ';', $json);
 					$json = str_replace(',', '","', $json);
 					$json = str_replace(';', '"],["', $json);
-									
+
 					echo $json;
-					
+
 					echo '</p>';
-					
+
 					$output_found = true;
-					
+
 					echo '<p id="board_solution">';
 				}
 				else if($output_found)
@@ -87,14 +90,14 @@ function run_create_board()
 						echo '</p>';
 						break;
 					}
-					
-					echo str_replace('))))', ')))', $results);	
+
+					echo str_replace('))))', ')))', $results);
 				}
 
 		}
-		
+
 		echo '</center></td></tr></table>';
-		
+
 		echo '<div id="output_display">Show Output</div>';
 		echo '<div id="acl2_output">';
 		var_dump ($console_log);
@@ -106,9 +109,9 @@ function run_create_board()
 	else
 	{
 		echo 'Enter a list of words to make a wordsearch. The words can be seperated by space, dash, comma, underscore, or newlines.<br /><br />';
-	
+
 		echo '<form method="get" action=""><center><textarea name="words">' . (isset ($_GET['words']) ? $_GET['words'] : 'Put your words here') . '</textarea>' .
-			'<br /><br /><input type="submit" value="Submit"></center></form>';	
+			'<br /><br /><input type="submit" value="Submit"></center></form>';
 	}
 }
 
